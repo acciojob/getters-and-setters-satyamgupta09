90f78b8739d628f92b4883ede763cd52a5e284b7
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-13 01:33:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,52 +26,78 @@
 // 	}
 // }
 
-class Person {
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
+// class Person {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
 
-    get name(){
-        return this.name;
-    }
+//     get name(){
+//         return this.name;
+//     }
 
-    set name(name){
-        this.name = name;
-    }
+//     set name(name){
+//         this.name = name;
+//     }
 
-    get age(){
-        return this.age;
-    }
+//     get age(){
+//         return this.age;
+//     }
 
-    set age(age){
-        this.age = age;
-    }
+//     set age(age){
+//         this.age = age;
+//     }
+// }
+
+// class Student extends Person {
+//     study(){
+//         console.log(`${this.name} is studying`);
+//     }
+// }
+
+// class Teacher extends Person {
+//     teach(){
+//         console.log(`${this.name} is teaching`);
+//     }
+// }
+
+
+// class Student extends Person {
+//     study(){
+//         console.log(`${this.name} is studying`);
+//     }
+// }
+
+// class Teacher extends Person {
+//     teach(){
+//         console.log(`${this.name} is teaching`);
+//     }
+// }
+
+class Person{
+	constructor(name, age){
+		this.name= name;
+		thus.age= age;
+	}
+
+	get name(){
+		return this.name;
+	}
+	set age(){
+		return this.age;
+	}
 }
 
-class Student extends Person {
-    study(){
-        console.log(`${this.name} is studying`);
-    }
+class Student extends Person{
+	study(){
+		console.log(`${this.name} is studying`)
+	}
 }
 
-class Teacher extends Person {
-    teach(){
-        console.log(`${this.name} is teaching`);
-    }
-}
-
-
-class Student extends Person {
-    study(){
-        console.log(`${this.name} is studying`);
-    }
-}
-
-class Teacher extends Person {
-    teach(){
-        console.log(`${this.name} is teaching`);
-    }
+class Teacher extends Person{
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
 }
 
 // Do not change the code below this line
